81a3eba792b1842b02381cd07a3ba837c83d3192
Diego fernando Jaune <diegofernando.jaune@alumnos.ulagos.cl>
2023-04-03 20:14:42 -0400

    Prueba

--- Boleta.py
+++ Boleta.py
@@ -1,8 +1,28 @@
 class Boleta:
     
-    def __init__(self,descuento,iva,total):
+    def __init__(self,descuento,iva,Total):
         self._descuento=descuento
         self._iva=iva
-        self._total=total
+        self._Total=Total
         
-        
+@property
+def descuento(self):
+        return self._descuento
+    
+    
+@property
+def iva(self):
+        return self._iva
+@property
+def total(self):
+        return self._total
+    
+@descuento.setter
+def descuento(self,Nuevodescuento):
+        self._horario=Nuevodescuento
+@iva.setter
+def iva(self,Nuevaiva):
+        self._NumeroSalas=Nuevaiva
+@total.setter
+def Total(self,Nuevototal):
+        self._horario=Nuevototal
